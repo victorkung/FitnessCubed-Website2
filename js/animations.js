b59d11b0e63c430890cb6f8f8fcbce7dcb9e12fe
Victor <victor.y.kung@gmail.com>
2014-11-20 11:58:21 -0600

    getting to testimonials page

--- js/animations.js
+++ js/animations.js
@@ -21,15 +21,13 @@ $(document).ready(function(){
 		slideSpeed : 300,
 		paginationSpeed : 400,
 		singleItem:true
-
-		// "singleItem:true" is a shortcut for:
-		// items : 1, 
-		// itemsDesktop : false,
-		// itemsDesktopSmall : false,
-		// itemsTablet: false,
-		// itemsMobile : false
 	 });
 
+	$("#testimony-slider").owlCarousel({
+		slideSpeed : 300,
+		paginationSpeed : 400,
+		singleItem:true
+	 });
 
 });
 
