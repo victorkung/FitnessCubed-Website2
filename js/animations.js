93a50491dd49017823676d2399f14b45b5182e3e
Victor <victor.y.kung@gmail.com>
2014-11-30 19:40:07 -0600

    made second round of changes based on feedback

--- js/animations.js
+++ js/animations.js
@@ -6,28 +6,55 @@ $(document).ready(function(){
 		$( this ).children( 'span' ).toggleClass("glyphicon-chevron-down glyphicon-chevron-up");
 	});
 
-	// Tooltip
-	$('#arnav').tooltip({'placement': 'bottom'});
-	$('#ryota').tooltip({'placement': 'bottom'});
-	$('#shivani').tooltip({'placement': 'bottom'});
-	$('#alida').tooltip({'placement': 'bottom'});
-	$('#mark').tooltip({'placement': 'bottom'});
-	$('#keith').tooltip({'placement': 'bottom'});
-	$('#robert').tooltip({'placement': 'bottom'});
-	$('#john').tooltip({'placement': 'bottom'});
 
 	// Owl Slider
 	$("#homepage-slider").owlCarousel({
 		slideSpeed : 300,
 		paginationSpeed : 400,
 		singleItem:true
-	 });
+		});
 
 	$("#testimony-slider").owlCarousel({
 		slideSpeed : 300,
 		paginationSpeed : 400,
 		singleItem:true
-	 });
+		});
+
+	$("#features-slider").owlCarousel({
+		slideSpeed : 300,
+		paginationSpeed : 400,
+		singleItem:true
+		});
+
+	$("#cubii-silver-select").click(function() {
+				$("#cubii-noir").fadeOut("slow", function() {
+					$("#cubii-noir").addClass('hidden');
+				});
+	
+					$("#cubii-silver").fadeIn("slow", function() {
+					$("#cubii-silver").removeClass('hidden');
+				});
+					
+					$("#cubii-silver-select").addClass('bold');
+					$("#cubii-noir-select").removeClass('bold');
+			});
+
+	$("#cubii-noir-select").click(function() {
+					$("#cubii-silver").fadeOut("slow", function() {
+					$( "#cubii-silver").addClass('hidden');
+				});
+		
+					$("#cubii-noir").fadeIn("slow", function() {
+					$( "#cubii-noir").removeClass('hidden');
+				});
+					$("#cubii-noir-select").addClass('bold');
+					$("#cubii-silver-select").removeClass('bold');
+			});
+
+	$('.video-lightbox').magnificPopup({ 
+		type: 'iframe'
+		// other options
+	});
 
 });
 
